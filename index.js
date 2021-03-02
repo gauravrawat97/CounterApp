a1536621db7e9d6b6e84210038b47b7443eab3a5
TTN <ttn@gaurav-rawat.iballbatonwifi.com>
2021-03-02 15:17:40 +0530

    style assignment

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Counter from './ClickCounter'
-AppRegistry.registerComponent(appName, () => Counter);
+import StyleComponent from './StyleComponent';
+AppRegistry.registerComponent(appName, () => StyleComponent);
