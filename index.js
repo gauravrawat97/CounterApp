b7e226615b1c9be2f5fc10fd40b56590d549edea
TTN <ttn@gaurav-rawat.iballbatonwifi.com>
2021-03-04 13:39:00 +0530

    change to buttonassignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/components/App';
 import {name as appName} from './app.json';
-import Counter from './ClickCounter'
+import Counter from './src/components/ClickCounter'
 AppRegistry.registerComponent(appName, () => Counter);
