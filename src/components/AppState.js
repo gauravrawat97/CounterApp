d07dfed2c61aa7f2b1206dd8cb59a130e0e3d3fe
TTN <emailrawat97@gmail.com>
2021-03-16 12:19:19 +0530

    s

---
+++ src/components/AppState.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+export default function Btn() {
+  return <></>;
+}
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: '#4f8080',
+    padding: 15,
+  },
+  btnTxt: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 15,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+});
